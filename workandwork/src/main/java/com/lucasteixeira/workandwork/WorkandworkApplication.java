aea890e59c4c93140379d0b95f9f4cc47cfd1158
Lukas-Rodrigo <lucasrodrigo0503@gmail.com>
2024-08-13 15:08:43 -0300

    primeiras configurações

---
+++ workandwork/src/main/java/com/lucasteixeira/workandwork/WorkandworkApplication.java
@@ -0,0 +1,13 @@
+package com.lucasteixeira.workandwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkandworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkandworkApplication.class, args);
+	}
+
+}
---
+++ workandwork/src/test/java/com/lucasteixeira/workandwork/WorkandworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasteixeira.workandwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkandworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
